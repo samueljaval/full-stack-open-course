9152c058d7fb42b3cf79c1c3da6a3fae703ecbcd
Samuel Javal <samueljaval@gmail.com>
2020-06-08 13:42:14 +0200

    part a done

---
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Notification = ({message, color}) => {
+    const NotifStyle = {
+    color: color,
+    fontStyle: 'italic',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    }
+
+  if (message === null) return null
+  return (
+      <div style={NotifStyle}>
+      <em>{message}</em>
+    </div>
+  )
+}
+
+export default Notification
